dc7402cbf67af8b313da8c251a476847ad7ced07
nickonyi <nickoluoch1130@gmail.com>
2024-01-11 20:53:13 +0300

    git

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import PackingList from './PackingList.jsx'
+import List from './List.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PackingList />
+    <List />
   </React.StrictMode>,
 )
